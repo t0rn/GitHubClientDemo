e1160e48759a5a4eccb9e1a32cfe544c349ab20f
Alexey Ivanov <aleksey.ivanov1@gmail.com>
2018-09-15 20:03:29 +0300

    Initial Commit

---
+++ SimpleGithubClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleGithubClient
+//
+//  Created by Alexey Ivanov on 15.09.2018.
+//  Copyright © 2018 Alexey Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
